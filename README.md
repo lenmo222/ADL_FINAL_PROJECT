fe62c2e6a0d2c00dc8d707e8cef32d7136f0fc1b
r10525062 <r10525062@ntu.edu.tw>
2023-09-23 15:33:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Env python 3.8
+
+### Please install package first
+```
+pip install -r requirements.txt
+bash run.sh
+```
+
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+gdown --folder https://drive.google.com/drive/folders/1vW22ia0XSlKRvMGisov00GZFhaU77qhd?usp=sharing
+python simulator.py --split test --num_chats 980 --model_name_or_path gpt_small
